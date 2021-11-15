e10ce1333728162639ddfc5ecf8294e97812c7bd
Wavy G <wavvvtron@gmail.com>
2021-11-14 20:23:31 -0500

    some other ones that were floating around

---
+++ logical.js
@@ -0,0 +1,6 @@
+const isCitizen = true;
+const age = 26;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
---
+++ whichSchool.js
@@ -0,0 +1,7 @@
+const whichSchool  = function (age) {
+  if (age < 13) {
+    console.log("Elementary School");
+}
+}
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
